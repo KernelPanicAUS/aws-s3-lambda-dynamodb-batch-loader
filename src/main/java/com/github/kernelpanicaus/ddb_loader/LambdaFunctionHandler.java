9cb346fa9839c61034abcdef994ad41902ae747f
Thomas Khalil <thomas@khalilt.com>
2021-01-21 21:29:38 +0100

    it works

--- src/main/java/com/github/kernelpanicaus/ddb_loader/LambdaFunctionHandler.java
+++ src/main/java/com/github/kernelpanicaus/ddb_loader/LambdaFunctionHandler.java
@@ -1,118 +1,188 @@
 package com.github.kernelpanicaus.ddb_loader;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.BatchWriteItemOutcome;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.TableWriteItems;
-import com.amazonaws.services.dynamodbv2.model.WriteRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.google.common.collect.Lists;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.BatchWriteItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutRequest;
+import software.amazon.awssdk.services.dynamodb.model.WriteRequest;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.utils.IoUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
-
 
 /**
  * The Class LambdaFunctionHandler.
  * This application loads GZIPped Dynamo JSON file to DynamoDB.
  */
-public class LambdaFunctionHandler implements RequestHandler<S3Event, Report> {
+public class LambdaFunctionHandler implements RequestStreamHandler {
 
     /**
      * Provide the AWS region which your DynamoDB table is hosted.
      */
-    static final String AWS_REGION = System.getenv("AWS_REGION");
+    static final Region AWS_REGION = Region.of(System.getenv("AWS_REGION"));
 
     /**
      * The DynamoDB table name.
      */
+    // TODO: Make this dynamic, from the S3 event.
     static final String DYNAMO_TABLE_NAME = System.getenv("DYNAMO_TABLE_NAME");
 
     /**
      * Configurable batch size
      */
-    static final int BATCH_SIZE = Integer.parseInt(System.getenv().getOrDefault("BATCH_SIZE","25"));
+    static final int BATCH_SIZE = Integer.parseInt(System.getenv().getOrDefault("BATCH_SIZE", "25"));
+//
+//    static final ClientConfiguration config = new ClientConfiguration()
+//            .withMaxConnections(ClientConfiguration.DEFAULT_MAX_CONNECTIONS * 2);
 
-    static final ClientConfiguration config = new ClientConfiguration()
-            .withMaxConnections(ClientConfiguration.DEFAULT_MAX_CONNECTIONS * 2);
-
-    final AmazonS3 s3Client = AmazonS3ClientBuilder
-            .standard()
-            .withClientConfiguration(config)
-            .withRegion(AWS_REGION)
+    final S3Client s3Client = S3Client.builder()
+            .region(AWS_REGION)
             .build();
 
-    final AmazonDynamoDB dynamoDBClient = AmazonDynamoDBClientBuilder
-            .standard()
-            .withClientConfiguration(config)
-            .withRegion(AWS_REGION)
+    final DynamoDbClient dynamoDBClient = DynamoDbClient.builder()
+            .region(AWS_REGION)
             .build();
 
-    final DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
+    static AttributeValue toAttributeValue(Object value) {
+        if (value == null) return AttributeValue.builder().nul(true).build();
+        if (value instanceof AttributeValue) return (AttributeValue) value;
+        if (value instanceof String) return AttributeValue.builder().s((String) value).build();
+        if (value instanceof Number) return AttributeValue.builder().n(value.toString()).build();
 
-    public Report handleRequest(S3Event s3event, Context context) {
+        if (value instanceof Map) return AttributeValue.builder().m(
+                ((Map<String, Object>) value).entrySet().stream().collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> toAttributeValue(e.getValue())
+                ))).build();
+
+        throw new UnsupportedOperationException("Time to impl new path for " + value);
+    }
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+        // TODO: Add sanity check for dynamo table
+        // TODO: Add dead letter queue for failures
+
         long startTime = System.currentTimeMillis();
         Report statusReport = new Report();
         LambdaLogger logger = context.getLogger();
-
-        logger.log("Lambda Function Started");
+        String event = null;
+        try {
+            event = IoUtils.toUtf8String(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.log(event + "\n");
+        logger.log("Lambda Function Started \n");
 
         try {
-            S3EventNotification.S3EventNotificationRecord record = s3event.getRecords().get(0);
-            String srcBucket = record.getS3().getBucket().getName();
-            String srcKey = record.getS3().getObject().getUrlDecodedKey();
 
-            S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
-            statusReport.setFileSize(s3Object.getObjectMetadata().getContentLength());
+            String srcKey = JsonPath.read(event, "$.Records[0].s3.object.key");
+            String srcBucket = JsonPath.read(event, "$.Records[0].s3.bucket.name");
 
-            logger.log("S3 Event Received: " + srcBucket + "/" + srcKey);
 
-            GZIPInputStream gis = new GZIPInputStream(s3Object.getObjectContent());
+            logger.log("Bucket name: " + srcBucket + "\n");
+            logger.log("Key name: " + srcKey + "\n");
+            logger.log("S3 Object: " + srcBucket + "/" + srcKey + "\n");
+
+            logger.log("S3 Event Received: " + srcBucket + "/" + srcKey + "\n");
+
+            ResponseInputStream<GetObjectResponse> responseInputStream = s3Client.getObject(
+                    GetObjectRequest.builder()
+                            .bucket(srcBucket)
+                            .key(srcKey)
+                            .build()
+                    , ResponseTransformer.toInputStream()
+            );
+
+            logger.log("Reading input stream \n");
+
+            GZIPInputStream gis = new GZIPInputStream(responseInputStream);
             Scanner fileIn = new Scanner(gis);
+            var parser = new JSONParser();
 
-            TableWriteItems energyDataTableWriteItems = new TableWriteItems(DYNAMO_TABLE_NAME);
+            Collection<WriteRequest> itemList = new ArrayList<>();
 
-            List<Item> itemList = new ArrayList<Item>();
-
+            int counter = 0;
+            int batchCounter = 0;
             while (fileIn.hasNext()) {
-                Item item = Item.fromJSON(fileIn.nextLine());
+                var line = fileIn.nextLine();
+//                logger.log(line + "\n");
+                JSONObject jsonLine = (JSONObject) parser.parse(line);
+                JSONObject jsonItem = (JSONObject) jsonLine.get("Item");
+//                var item = jsonLine.getJSONObject("Item");
+
+//                jsonLine.get("Item")
+//                HashMap<String, AttributeValue> result = (HashMap<String, AttributeValue>) JacksonUtils.fromJsonString(line, HashMap.class).get("Item")
+//                        .entrySet()
+//                        .stream()
+//                        .collect(Collectors.toMap(
+//                                entry -> entry.toString(),
+//                                entry -> entry
+//                        ));
+
+//                logger.log(jsonItem.toString() + "\n");
+//                logger.log(jsonItem.getClass().toString() + "\n");
+
+                WriteRequest item = WriteRequest.builder()
+                        .putRequest(PutRequest.builder().item(jsonItem).build())
+                        .build();
+
                 itemList.add(item);
+
+                logger.log("[" + batchCounter + "/" + counter + "] Adding item to itemlist \n");
+                counter++;
+
+                if (counter == BATCH_SIZE) {
+                    batchCounter++;
+
+                    var batchItemRequest = BatchWriteItemRequest.builder()
+                            .requestItems(Map.of(DYNAMO_TABLE_NAME, itemList))
+                            .build();
+
+                    logger.log("Sending Batch "+ batchCounter +" \n");
+//                    logger.log(batchItemRequest.toString() + "\n");
+
+                    var outcome = dynamoDBClient.batchWriteItem(batchItemRequest);
+
+                    do {
+                        var unprocessedItemsRequest = BatchWriteItemRequest.builder()
+                                .requestItems(outcome.unprocessedItems())
+                                .build();
+
+                        if (outcome.unprocessedItems().size() > 0) {
+                            logger.log("Retrieving the unprocessed " + outcome.unprocessedItems().size() + " items, batch ["+ batchCounter+"].");
+                            outcome = dynamoDBClient.batchWriteItem(unprocessedItemsRequest);
+                        }
+
+                    } while (outcome.unprocessedItems().size() > 0);
+                    itemList.clear();
+                    counter = 0;
+                }
             }
 
-            for (List<Item> partition : Lists.partition(itemList, BATCH_SIZE)) {
-                energyDataTableWriteItems.withItemsToPut(partition);
-                BatchWriteItemOutcome outcome = dynamoDB.batchWriteItem(energyDataTableWriteItems);
-
-                do {
-
-                    Map<String, List<WriteRequest>> unprocessedItems = outcome.getUnprocessedItems();
-
-                    if (outcome.getUnprocessedItems().size() > 0) {
-                        logger.log("Retrieving the unprocessed " + outcome.getUnprocessedItems().size() + " items.");
-                        outcome = dynamoDB.batchWriteItemUnprocessed(unprocessedItems);
-                    }
-
-                } while (outcome.getUnprocessedItems().size() > 0);
-            }
 
             logger.log("Load finish in " + (System.currentTimeMillis() - startTime) + "ms");
             fileIn.close();
             gis.close();
-            s3Object.close();
 
             statusReport.setStatus(true);
         } catch (Exception ex) {
@@ -120,6 +190,5 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, Report> {
         }
 
         statusReport.setExecutiongTime(System.currentTimeMillis() - startTime);
-        return statusReport;
     }
 }
